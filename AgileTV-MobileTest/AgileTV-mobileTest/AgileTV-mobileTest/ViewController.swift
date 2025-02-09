c160117f497b84e6a0b10cc983d697331f5fa2d8
enrique-sem-h <carvalhoenrique33@gmail.com>
2025-02-09 03:33:09 -0300

    Initial project config

---
+++ AgileTV-MobileTest/AgileTV-mobileTest/AgileTV-mobileTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AgileTV-mobileTest
+//
+//  Created by Enrique on 09/02/25.
+//  Copyright © 2025 Enrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
